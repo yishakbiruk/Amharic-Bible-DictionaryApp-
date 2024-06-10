3831cdac9a34c7cf35f86f3f3b01e9788cb69c72
Yishak Aemro <yishakbiruk25@gmail.com>
2024-06-10 10:40:39 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+
+module.exports = {
+   content: ["./app.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}","./app/.index.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
